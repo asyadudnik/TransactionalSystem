fe562c0afa57070032ee7e6ffb4a9fe18e3dcdf8
Oksana Dudnik <123472443+asyadudnik@users.noreply.github.com>
2024-02-22 14:25:51 +0200

    Revert "Test"
    This reverts commit 8f00034be8bd70bc92562b7b95f5032fe53bf090.

--- src/main/java/com/optum/payment/system/repositories/UserRepository.java
+++ src/main/java/com/optum/payment/system/repositories/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
     User findByEmail(String email);
     User findByEmailAndPassword(String email, String password);
     Optional<User> findByLogin(String username);
