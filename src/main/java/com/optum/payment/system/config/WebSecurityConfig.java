d03751c2d34b943ff8c1a404b9cd8ee1fa54a790
Oksana Dudnik <123472443+asyadudnik@users.noreply.github.com>
2024-02-22 14:25:02 +0200

    Revert "Test"
    This reverts commit 81b5ed88c03ece7af78fa160e2208b5aa0ab4521.

--- src/main/java/com/optum/payment/system/config/WebSecurityConfig.java
+++ src/main/java/com/optum/payment/system/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.optum.payment.system.config;
 
 import com.optum.payment.system.entities.enums.RoleName;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,19 +44,14 @@ public class WebSecurityConfig {
         return new InMemoryUserDetailsManager(springuser, root);
     }
 
-
-/*
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       // http.securityMatcher(EndpointRequest.toAnyEndpoint());
+        http.securityMatcher(EndpointRequest.toAnyEndpoint());
         http.authorizeHttpRequests(requests -> requests
-                .requestMatchers("/payment/api/users")
-                .hasRole("ADMIN")
-                ).build();
-        //http.httpBasic(withDefaults());
+                .requestMatchers("/user/login/")
+                .hasRole("ADMIN")).build();
+        http.httpBasic(withDefaults());
         return http.build();
     }
-
-*/
 
 }
